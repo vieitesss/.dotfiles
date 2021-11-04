fb530f7f57a5b6a38eea6264dbd15bb5d72a599c
Daniel Vieites <daniel.vieites.torres@udc.es>
2021-11-04 18:51:05 +0100

    install vim-plug

--- setup/default.sh
+++ setup/default.sh
@@ -2,6 +2,9 @@ packages=(vim curl nodejs)
 for i in ${packages[@]}
 do
     printf "\nINSTALLING $i\n"
-    sleep 
+    sleep 2
     sudo apt install $i -y
 done
+
+printf "\nINSTALLING vim-plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
