01bcccec466f4404911ef6c31a31d4c72eed4344
Daniel Vieites <daniel.vieites.torres@udc.es>
2021-11-15 20:33:34 +0100

    aliases, .bashrc y .profile

---
+++ aliases/general.aliases.bash
@@ -0,0 +1,90 @@
+
+if ls --color -d . &> /dev/null
+then
+  alias ls="ls --color=auto"
+elif ls -G -d . &> /dev/null
+then
+  alias ls='ls -G'        # Compact view, show colors
+fi
+
+# List directory contents
+alias sl=ls
+alias la='ls -AF'       # Compact view, show hidden
+alias ll='ls -al'
+alias l='ls -a'
+alias l1='ls -1'
+
+alias _="sudo"
+
+# Shortcuts to edit startup files
+alias vbrc="vim ~/.bashrc"
+alias vbpf="vim ~/.bash_profile"
+
+# colored grep
+# Need to check an existing file for a pattern that will be found to ensure
+# that the check works when on an OS that supports the color option
+if grep --color=auto "a" "${BASH_IT}/"*.md &> /dev/null
+then
+  alias grep='grep --color=auto'
+fi
+
+
+alias c='clear'
+alias k='clear'
+alias cls='clear'
+
+alias edit="$EDITOR"
+alias pager="$PAGER"
+
+alias q='exit'
+
+alias irc="${IRC_CLIENT:=irc}"
+
+# Language aliases
+alias rb='ruby'
+alias py='python'
+alias ipy='ipython'
+
+# Pianobar can be found here: http://github.com/PromyLOPh/pianobar/
+
+alias piano='pianobar'
+
+alias ..='cd ..'         # Go up one directory
+alias cd..='cd ..'       # Common misspelling for going up one directory
+alias ...='cd ../..'     # Go up two directories
+alias ....='cd ../../..' # Go up three directories
+alias -- -='cd -'        # Go back
+
+# Shell History
+alias h='history'
+
+# Directory
+alias md='mkdir -p'
+alias rd='rmdir'
+
+# Shorten extract
+alias xt="extract"
+
+# sudo editors
+alias svim="sudo vim"
+alias snano="sudo nano"
+
+# Display whatever file is regular file or folder
+catt() {
+  for i in "$@"; do
+    if [ -d "$i" ]; then
+      ls "$i"
+    else
+      cat "$i"
+    fi
+  done
+}
+
+# The Bash-it aliases were moved to the `bash-it.aliases.bash` file. The intent of this
+# is to keep the script readable and less bloated. If you don't need to use
+# the `general` aliases, but you want the Bash-it aliases, you can disable the `general`
+# aliases and enable just the ones for Bash-it explicitly:
+# bash-it disable alias general
+# bash-it enable alias bash-it
+# shellcheck source=./bash-it.aliases.bash
+#source "$BASH_IT/aliases/available/bash-it.aliases.bash"
