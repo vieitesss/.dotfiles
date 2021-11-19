84289adbc9fe2f14f8a1e41ef5c186adc812daa9
Daniel Vieites <daniel.vieites.torres@udc.es>
2021-11-19 13:31:36 +0100

    config polybar

--- polybar/.config/polybar/forest/scripts/powermenu.sh
+++ polybar/.config/polybar/forest/scripts/powermenu.sh
@@ -82,6 +82,8 @@ case $chosen in
 				openbox --exit
 			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
 				bspc quit
+			elif [[ "$DESKTOP_SESSION" == "awesome" ]]; then
+                echo 'awesome.quit()' | awesome-client
 			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
 				i3-msg exit
 			fi
