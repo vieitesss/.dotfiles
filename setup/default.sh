7dd52169ec9e76b3e89b075c4c869bfd7d38eb3a
Daniel Vieites <daniel.vieites.torres@udc.es>
2021-11-23 11:25:22 +0100

    scripts

--- setup/default.sh
+++ setup/default.sh
@@ -18,6 +18,10 @@ declare -A packages=(
 ["polybar"]="polybar"
 )
 
+function installed(){
+    printf "%-10s ALREADY EXISTS\n" "$1"
+}
+
 for i in ${!packages[@]}
 do
     existe=$(which ${packages[$i]})
@@ -26,7 +30,7 @@ do
         sleep 2
         sudo apt install ${packages[$i]} -y
     else
-        printf "%-10s ALREADY EXISTS\n" $i
+        installed "$i"
     fi
 done
 
@@ -34,12 +38,20 @@ if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
     printf "\nINSTALLING vim-plug"
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 else
-    printf "%-10s ALREADY EXISTS\n" "vim-plug"
+    installed "vim-plug"
 fi
 
 if [[ ! -d ~/.tmux/plugins/tpm ]]; then
     printf "\nINSTALLING tmux plugin manager\n"
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 else
-    printf "%-10s ALREADY EXISTS\n" "tpm "
+    installed "tpm"
 fi
+
+if [[ -z $(which starship) ]]; then
+    printf "\nINSTALLING starship\n"
+    sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+else
+    installed "starship"
+fi
+
--- setup/install-colorscript.sh
+++ setup/install-colorscript.sh
@@ -2,7 +2,7 @@
 
 echo "INSTALLING colorscript"
 
-git clone https://gitlab.com/dwtl/shell-color-scripts.git
+git clone https://gitlab.com/dwt1/shell-color-scripts.git
 cd shell-color-scripts
 rm -rf /opt/shell-color-scripts || return 1
 sudo mkdir -p /opt/shell-color-scripts/colorscripts || return 1
