9619e6937b556c04ea20d29d3573e191fa5adaaf
Daniel Vieites <daniel.vieites.torres@udc.es>
2021-11-25 19:58:12 +0100

    .sh updated

--- setup/install-colorscript.sh
+++ setup/install-colorscript.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo "INSTALLING colorscript"
-
-git clone https://gitlab.com/dwt1/shell-color-scripts.git
-cd shell-color-scripts
-rm -rf /opt/shell-color-scripts || return 1
-sudo mkdir -p /opt/shell-color-scripts/colorscripts || return 1
-sudo cp -rf colorscripts/* /opt/shell-color-scripts/colorscripts
-sudo cp colorscript.sh /usr/bin/colorscript
+if [[ -z $(which colorscript) ]]; then
+    git clone https://gitlab.com/dwt1/shell-color-scripts.git
+    cd shell-color-scripts
+    rm -rf /opt/shell-color-scripts || return 1
+    sudo mkdir -p /opt/shell-color-scripts/colorscripts || return 1
+    sudo cp -rf colorscripts/* /opt/shell-color-scripts/colorscripts
+    sudo cp colorscript.sh /usr/bin/colorscript
+else
+    printf "%-10s ALREADY INSTALLED\n" "colorscript"
+fi
--- setup/install-yarn.sh
+++ setup/install-yarn.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-if hash yarn 2>/dev/null; then
-    printf "yarn ALREADY EXISTS"
-else
+if [[ -z $(which yarn) ]]; then
     printf "\nINSTALLING yarn"
-    sleep 2
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     sudo apt install yarn -y
+else
+    printf "yarn ALREADY EXISTS\n"
 fi
