97c90b240766b5f3dcbb89ef98f4604082a07113
Daniel Vieites <daniel.vieites.torres@udc.es>
2021-12-09 12:35:29 +0100

    install python updated

--- setup/install-python.sh
+++ setup/install-python.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if hash python3-pip 2>/dev/null; then
-    printf "pip3 ALREADY EXISTS"
-else
+if [[ -z $(which pip3) ]]; then
     sudo apt install python3-pip -y
+    pip3 install --user pynvim
+else
+    printf "pip3 ALREADY EXISTS\n"
 fi
-pip3 install --user pynvim
