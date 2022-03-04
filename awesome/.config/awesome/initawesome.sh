c5e9a17af13ef7c13ff7e23dcb7e95f355438666
Daniel Vieites Torres <dvieitest@gmail.com>
2022-03-04 17:28:23 +0100

    initawesome.sh

--- awesome/.config/awesome/initawesome.sh
+++ awesome/.config/awesome/initawesome.sh
@@ -13,3 +13,4 @@ pgrep -u $USER -x nm-applet > /dev/null || (nm-applet &)
 ibus-daemon
 
 sudo chmod 666 "/sys/class/backlight/acpi_video0/brightness"
+sudo chmod 666 "/sys/class/leds/apple\:\:kbd_backlight/brightness"
