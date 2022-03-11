a9c266714fde265c0565dbe7a2220589aafa9ec3
Daniel Vieites Torres <dvieitest@gmail.com>
2022-03-11 08:36:04 +0100

    mac with monitor

--- defaultdots/.mac.sh
+++ defaultdots/.mac.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-xrandr --output eDP --mode 1920x1200 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off
+#!/bin/bash
+
+if [[ "$(xrandr --query | grep " connected " | wc -l)" = 2 ]]; then
+    xrandr --output eDP --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --mode 1920x1080 --pos 0x120 --rotate normal --output DisplayPort-3 --off
+else
+    xrandr --output eDP --mode 1920x1200 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off
+fi
--- polybar/.config/polybar/forest/scripts/bat_alarm.sh
+++ polybar/.config/polybar/forest/scripts/bat_alarm.sh
@@ -12,9 +12,12 @@ if [ "$number" -lt "3" ]; then
                 rofi -theme $dir/data.rasi -e " ${battery_data[3]}%"
                 sleep 120
             else
-                echo "mayor"
                 sleep 300
             fi
+        elif [ "${battery_data[3]}" -eq "100" ]; then
+            sed -i -r "s#width.*#width:120px;#" $dir/data.rasi
+            rofi -theme $dir/data.rasi -e " ${battery_data[3]}%"
+            sleep 120
         else
             sleep 300
         fi
