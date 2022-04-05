f81f6ea7237abdf9feb94a58f6552732bd56cea8
Daniel Vieites Torres <dvieitest@gmail.com>
2022-04-05 23:47:29 +0200

    polybar styles script updated

--- polybar/.config/polybar/scripts/style-switch.sh
+++ polybar/.config/polybar/scripts/style-switch.sh
@@ -1,12 +1,30 @@
 #!/usr/bin/env bash
 
-SDIR="$HOME/.config/polybar/scripts"
+DIR="$HOME/.config/polybar"
+SDIR="$DIR/scripts"
+ROFI="$SDIR/rofi"
+
+set-style ()
+{
+    WM=$(wmctrl -m | head -n 1 | awk '{print $2}')
+
+    if  [[ $1 = "--default" ]]; then
+        ln -fs "$DIR"/forest/colors.ini "$DIR"/colors.ini 
+        ln -fs "$DIR"/forest/colors.rasi "$ROFI"/colors.rasi 
+        "$DIR"/launch.sh "$WM"
+    elif  [[ $1 = "--dracula" ]]; then
+        ln -fs  "$DIR"/dracula/colors.ini "$DIR"/colors.ini
+        ln -fs "$DIR"/dracula/colors.rasi "$ROFI"/colors.rasi
+        "$DIR"/launch.sh "$WM"
+    fi
+}
 
 # Launch Rofi
 MENU="$(rofi -no-config -no-lazy-grab -sep "|" -dmenu -i -p '' \
--theme $SDIR/rofi/styles.rasi \
+-theme $ROFI/styles.rasi \
 <<< " Default| Dracula|")"
             case "$MENU" in
-				*Default) "$SDIR"/styles.sh --default ;;
-				*Dracula) "$SDIR"/styles.sh --dracula ;;
+                *Default) set-style --default ;;
+                *Dracula) set-style --dracula ;;
+                *) rofi -theme "$ROFI/message.rasi" -e "Not valid option"
             esac
