3b424ac5f384ccc740d8a85cd0af4ec894138df1
Daniel Vieites Torres <dvieitest@gmail.com>
2022-04-05 23:31:45 +0200

    me olvidé de guardar el archivo sorry

--- polybar/.config/polybar/scripts/styles.sh
+++ polybar/.config/polybar/scripts/styles.sh
@@ -4,14 +4,6 @@ DIR="$HOME/.config/polybar"
 ROFI="$HOME/.config/polybar/scripts/rofi"
 WM=$(wmctrl -m | head -n 1 | awk '{print $2}')
 
-if [[ "$WM" = "bspwm" ]]; then
-    echo "hola"
-    # $WM="--bspwm"
-elif [[ "$WM" = "awesome" ]]; then
-    echo "adios"
-    # $WM="--awesome"
-fi
-
 if  [[ $1 = "--default" ]]; then
     ln -fs "$DIR"/forest/colors.ini "$DIR"/colors.ini 
     ln -fs "$DIR"/forest/colors.rasi "$ROFI"/colors.rasi 
