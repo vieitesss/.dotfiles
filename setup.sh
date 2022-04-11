f4bd9e2d6f25f4172fe62f50b1f91f06e7f9e7dc
Daniel Vieites Torres <dvieitest@gmail.com>
2022-04-11 03:12:15 +0200

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,94 @@
 #!/bin/bash
 
-for file in $DOTFILES/setup/*;
-do
-    printf "\nEXECUTING FILE $file\n"
-    $file
+RED='\033[0;31m'
+NC='\033[0m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+MAGENTA='\033[1;35m'
+GREY='\033[1;37m'
+ORANGE='\033[33m'
+
+trap ctrl_c INT
+
+ctrl_c ()
+{
+    printf "${RED}Exiting...${NC}\n"
+    tput cnorm; exit 1
+}
+
+error ()
+{
+    printf "\n${RED}$1${NC}\n"
+    tput cnorm
+    exit 1;
+}
+
+install-basics ()
+{
+basics=("cargo" "bat" "flameshot" "git" "ibus" "stow" "bspwm" "sxhkd" "curl" "wget")
+
+for b in "${basics[@]}"; do
+    if ! command -v $b > /dev/null; then
+        printf "${BLUE}%-10s ${RED}%3s${NC}\n" $b "[X]"
+        printf "${ORANGE}Installing $b... ${NC}\n" $b
+
+        apt install "$b" > /dev/null 2>&1
+
+        if [[ $? != 0 ]]; then
+            error "Error installing $b"
+        else
+            printf "${BLUE}%-10s ${GREEN}%3s${NC}\n" $b "[V]"
+        fi
+    else
+        printf "${BLUE}%-10s ${GREEN}%3s${NC}\n" $b "[V]"
+    fi
+    # sleep 0.5
 done
 
-echo "EXECUTE :PlugInstall in vim"
-echo "EXECUTE Ctrl+B I in tmux"
+printf "\n${GREEN}All basics installed!${NC}\n\n"
+}
+
+install-alacritty ()
+{
+    if ! command -v rustc > /dev/null; then
+        printf "${ORANGE}Installing rust... ${NC}\n"
+        printf "${GREY}It could take some minutes, please be patient.${NC}\n"
+        
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        rustup override set stable
+        rustup update stable
+
+        printf "${ORANGE}Installing alacritty dependencies... ${NC}\n"
+
+        apt install cmake pkag-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 > /dev/null 2>&1
+
+        printf "${ORANGE}Installing alacritty... ${NC}\n"
+
+        cargo install alacritty
+
+        git clone https://github.com/alacritty/alacritty.git ~/
+        cd alacritty
+        cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+        desktop-file-install extra/linux/Alacritty.desktop
+        update-desktop-database
+    fi
+
+    printf "${BLUE}%-10s ${GREEN}%3s${NC}\n" "alacritty" "[V]"
+}
+
+main ()
+{
+    tput civis
+
+    install-basics 
+    install-alacritty
+
+    tput cnorm
+}
+
+if [[ "$(id -u)" != "0" ]]; then
+    printf "${RED}Please run as root${NC}\n"
+    exit 0
+fi
+
+main
