8698b7c3af46b1512ab3a039a6922153cc5462e2
Daniel Vieites Torres <dvieitest@gmail.com>
2022-09-19 13:29:05 +0200

    makefile passing target variable, nvim cmp disable when writting comments

--- nvim/.config/nvim/after/plugin/cmp.lua
+++ nvim/.config/nvim/after/plugin/cmp.lua
@@ -10,79 +10,90 @@ local has_words_before = function()
 end
 
 local feedkey = function(key, mode)
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+-- Disable cmp when writting comments
+local enabled = function()
+  local in_prompt = vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt'
+  if in_prompt then
+    return false
+  end
+  local context = require("cmp.config.context")
+  return not (context.in_treesitter_capture("comment") == true or context.in_syntax_group("Comment"))
 end
 
 cmp.setup {
-    snippet = {
-        expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            -- luasnip.lsp_expand(args.body)
-        end,
+  enabled = enabled,
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- luasnip.lsp_expand(args.body)
+    end,
+  },
+  window = {
+    documentation = cmp.config.window.bordered()
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-l>"] = cmp.mapping(function(fallback)
+      -- if cmp.visible() then
+      --     cmp.close()
+      if vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<C-j>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<C-k>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
+    ["<Tab>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
     },
-    window = {
-        documentation = cmp.config.window.bordered()
-    },
-    mapping = cmp.mapping.preset.insert({
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-l>"] = cmp.mapping(function(fallback)
-            -- if cmp.visible() then
-            --     cmp.close()
-            if vim.fn["vsnip#available"](1) == 1 then
-                feedkey("<Plug>(vsnip-expand-or-jump)", "")
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<C-j>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif has_words_before() then
-                cmp.complete()
-            else 
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<C-k>"] = cmp.mapping(function()
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                feedkey("<Plug>(vsnip-jump-prev)", "")
-            end
-        end, { "i", "s" }),
-        ["<Tab>"] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-        },
-        ["<c-space>"] = cmp.mapping.complete(),
-    }),
-    sources = cmp.config.sources({
-        { name = 'vsnip'},
-        { name = 'nvim_lsp', max_item_count = 6},
-        { name = 'path'},
-        -- { name = 'luasnip'},
-    },
+    ["<c-space>"] = cmp.mapping.complete(),
+  }),
+  sources = cmp.config.sources({
+    { name = 'vsnip' },
+    { name = 'nvim_lsp', max_item_count = 6 },
+    { name = 'path' },
+    -- { name = 'luasnip'},
+  },
     {
-        {name = 'buffer'}
+      { name = 'buffer' }
     }),
-    formatting = {
-        format = lspkind.cmp_format {
-            with_text = true,
-            menu = {
-                vsnip = "[VSnip]",
-                nvim_lsp = "[LSP]",
-                buffer = "[BUF]"
-                -- luasnip = "[LuaSnip]",
-            }
-        }
-    },
-    confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
-    },
-    experimental = {
-        -- native_menu = false,
-        ghost_text = true,
-    },
+  formatting = {
+    format = lspkind.cmp_format {
+      with_text = true,
+      menu = {
+        vsnip = "[VSnip]",
+        nvim_lsp = "[LSP]",
+        buffer = "[BUF]"
+        -- luasnip = "[LuaSnip]",
+      }
+    }
+  },
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
+  },
+  experimental = {
+    -- native_menu = false,
+    ghost_text = true,
+  },
 }
--- nvim/.config/nvim/lua/vt/code_runner.lua
+++ nvim/.config/nvim/lua/vt/code_runner.lua
@@ -6,6 +6,6 @@ require("code_runner").setup {
   filetype = {
     python = "python3 $file",
     java = "cd $dir && javac $fileName && java $fileNameWithoutExt && rm $fileNameWithoutExt.class",
-    c = "cd $dir && make -f ~/.dotfiles/c/Makefile"
+    c = "cd $dir && make -f ~/.dotfiles/c/Makefile target=$fileNameWithoutExt"
   }
 }
