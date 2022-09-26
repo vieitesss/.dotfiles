af43dd49565b6435f32265563b2ca2ad1ebe2640
Daniel Vieites Torres <dvieitest@gmail.com>
2022-09-26 18:27:45 +0200

    language servers file deleted

--- nvim/.config/nvim/language-servers.sh
+++
@@ -1,9 +0,0 @@
-# python
-sudo npm i -g pyright
-# json
-sudo npm i -g vscode-langservers-extracted
-# c
-sudo apt install clangd-11
-# latex
-wget "https://github.com/valentjn/ltex-ls/releases/download/15.2.0/ltex-ls-15.2.0-linux-x64.tar.gz"
-
